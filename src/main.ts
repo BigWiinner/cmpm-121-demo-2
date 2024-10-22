65d13f9db78ba03a3d16f928ae3a46ed5ddfb3c7
BigWiinner <150750398+BigWiinner@users.noreply.github.com>
2024-10-22 00:12:47 +0000

    Step 1 complete

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,16 @@
 import "./style.css";
 
-const APP_NAME = "My Sketchpad";
+const APP_NAME = "My Spooky Sketchpad!";
 const app = document.querySelector<HTMLDivElement>("#app")!;
 
-document.title = APP_NAME;
-app.innerHTML = APP_NAME;
+const title = document.createElement("h1");
+title.innerHTML = APP_NAME;
+app.append(title);
+
+const canvas = document.getElementById("canvas") as HTMLCanvasElement;
+const ctx = canvas.getContext("2d");
+
+if (ctx) {
+    ctx.fillStyle = "orange";
+    ctx.fillRect(0, 0, 256, 256);
+}
